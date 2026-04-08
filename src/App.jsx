5effb90bc336d2a8289eb9bc315d7c3d8f16aed3
Prionta Saha Dip <pksaha500@gmail.com>
2026-04-09 01:38:39 +0600

    added the main segment to the github

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+
+import './App.css'
+import Countries from './components/Countries/Countries'
+
+function App() {
+  
+
+  return (
+    <>
+      <h3>Prionta</h3>
+      <Countries></Countries>
+    </>
+  )
+}
+
+export default App
